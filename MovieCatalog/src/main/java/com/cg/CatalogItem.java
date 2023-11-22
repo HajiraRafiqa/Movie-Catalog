bc30cafd00bfe0d6bfa73d406898e39ce42302db
HajiraRafiqa <101549958+HajiraRafiqa@users.noreply.github.com>
2023-11-22 13:00:04 +0530

    adding code

---
+++ MovieCatalog/src/main/java/com/cg/CatalogItem.java
@@ -0,0 +1,16 @@
+package com.cg;
+
+public class CatalogItem {
+	
+	private String name;
+    private String desc;
+    private int rating;
+
+    public CatalogItem(String name, String desc, int rating) {
+        this.name = name;
+        this.desc = desc;
+        this.rating = rating;
+    }
+
+
+}
